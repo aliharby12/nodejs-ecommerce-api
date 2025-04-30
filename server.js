9f4cc733272a087f547a047bdcbcb09dfd3e029f
aliharby12 <aliharby565@gmail.com>
2025-04-30 17:42:54 +0300

    feat: add morgan middleware for logging in development mode

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+const morgan = require('morgan');
+
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+    console.log(`Mode is: ${process.env.NODE_ENV} mode`);
+}
 
 const PORT = process.env.PORT || 3000;
 
