8907c078a8a4bed8bf1b359cd5a02af6242037de
aliharby12 <aliharby565@gmail.com>
2025-05-04 14:05:23 +0300

    feat: refactor error handling to separate development and production responses

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,10 +2,28 @@ const errorHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
+    if (process.env.NODE_ENV === 'development') {
+        return handleDevelopmentError(err, res);
+    }
+    else {
+        return handleProductionError(err, res);
+    }
+}
+
+// In development, we want to expose the error stack trace
+const handleDevelopmentError = (err, res) => {
+    return res.status(err.statusCode).json({
         status: err.status,
         message: err.message,
-        stack: process.env.NODE_ENV === 'development' ? (typeof err.stack === 'string' ? err.stack.split('\n').map(line => line.trim()) : []) : {},
+        stack: err.stack ? err.stack.split('\n').map(line => line.trim()) : []
+    });
+}
+
+// In production, we don't want to expose the error stack trace
+const handleProductionError = (err, res) => {
+    return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
     });
 }
 
